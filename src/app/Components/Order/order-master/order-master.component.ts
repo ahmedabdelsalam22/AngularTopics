d1d8a7da93cec6cf54475ca6e88bca5a9703df57
prAhmed20 <ttbyqatandrwydmhkrh@gmail.com>
2023-12-22 01:10:35 +0200

    Components @Viewchild

--- src/app/Components/Order/order-master/order-master.component.ts
+++ src/app/Components/Order/order-master/order-master.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { AfterViewInit, Component, ElementRef, ViewChild } from '@angular/core';
 import { ICategory } from 'src/app/Models/icategory';
 import { IProduct } from 'src/app/Models/iproduct';
 
@@ -7,17 +7,23 @@ import { IProduct } from 'src/app/Models/iproduct';
   templateUrl: './order-master.component.html',
   styleUrls: ['./order-master.component.scss'],
 })
-export class OrderMasterComponent {
+export class OrderMasterComponent implements AfterViewInit {
   catList: ICategory[];
   selectedCatId: number = 0;
 
   recievedOrderTotalPrice: number = 0;
+
+  @ViewChild('clientNameInp') clientNameInpElem!: ElementRef;
 
   constructor() {
     this.catList = [
       { id: 1, name: 'Labs' },
       { id: 2, name: 'PC' },
     ];
+  }
+  ngAfterViewInit(): void {
+    this.clientNameInpElem.nativeElement.value = 'ahmed abd elsalam';
+    this.clientNameInpElem.nativeElement.style.border = '2px solid red';
   }
 
   OnTotalPriceChange(totalPrice: number) {
