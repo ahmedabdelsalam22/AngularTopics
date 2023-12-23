efd419d20780ca82276e00e376f1f7c3e8a36906
prAhmed20 <ttbyqatandrwydmhkrh@gmail.com>
2023-12-23 18:12:30 +0200

    create observer to subscribe on AdsObservable

--- src/app/Components/home/home.component.ts
+++ src/app/Components/home/home.component.ts
@@ -1,20 +1,39 @@
-import { Component } from '@angular/core';
+import { Observable } from 'rxjs';
+import { Component, OnInit } from '@angular/core';
+import { PromotionAdsService } from 'src/app/Services/promotion-ads.service';
 import { StoreData } from 'src/app/ViewModels/store-data';
 
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
-  styleUrls: ['./home.component.scss']
+  styleUrls: ['./home.component.scss'],
 })
-export class HomeComponent {
+export class HomeComponent implements OnInit {
+  storeInfo: StoreData;
 
- storeInfo:StoreData;
+  /**
+   *
+   */
+  constructor(private adsService: PromotionAdsService) {
+    this.storeInfo = new StoreData(
+      'ITI',
+      'https://picsum.photos/200/300/?blur',
+      ['cairo', 'beni suef', 'giza']
+    );
+  }
+  ngOnInit(): void {
+    let Observer = {
+      next: (data: string) => {
+        console.log(data);
+      },
+      error: (err: string) => {
+        console.log(err);
+      },
+      complete: () => {
+        console.log('ads finished');
+      },
+    };
 
- /**
-  *
-  */
- constructor() {
-  this.storeInfo = new StoreData('ITI','https://picsum.photos/200/300/?blur',['cairo','beni suef','giza']);
- }
-
+    this.adsService.getScheduledAds(3).subscribe(Observer);
+  }
 }
