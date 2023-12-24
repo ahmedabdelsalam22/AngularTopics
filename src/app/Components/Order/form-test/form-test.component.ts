d1823a5b5a00c981e34580089651d75087f6cbf2
prAhmed20 <ttbyqatandrwydmhkrh@gmail.com>
2023-12-24 20:14:34 +0200

    model Binding in forms

--- src/app/Components/Order/form-test/form-test.component.ts
+++ src/app/Components/Order/form-test/form-test.component.ts
@@ -1,10 +1,19 @@
 import { Component } from '@angular/core';
+import { NgForm } from '@angular/forms';
+import { LoginModel } from 'src/app/Models/login';
 
 @Component({
   selector: 'app-form-test',
   templateUrl: './form-test.component.html',
-  styleUrls: ['./form-test.component.scss']
+  styleUrls: ['./form-test.component.scss'],
 })
 export class FormTestComponent {
+  public loginModel: LoginModel;
+  constructor() {
+    this.loginModel = new LoginModel();
+  }
 
+  onSubmit(form: NgForm) {
+    console.log(this.loginModel);
+  }
 }
---
+++ src/app/Models/login.ts
@@ -0,0 +1,6 @@
+export class LoginModel {
+  public id?: number;
+  public email?: string;
+  public password?: string;
+  constructor() {}
+}
